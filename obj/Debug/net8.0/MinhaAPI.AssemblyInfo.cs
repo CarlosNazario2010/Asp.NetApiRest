490c9dfb4d78f34a99d0e7c900afdaa2096cc525
Carlos Nazario <carlosnazario2010cn@gmail.com>
2025-01-14 20:29:42 -0300

    Deletando a pasta de prints de tela desnecessarias

--- obj/Debug/net8.0/MinhaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinhaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4aa3ea2bd847664737832cb286aa93ce6ec6ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc36ab708cc6c7dfd82fe20ce05e87cdc21a716")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
